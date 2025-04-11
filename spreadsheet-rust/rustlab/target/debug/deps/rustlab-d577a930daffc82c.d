1e05248956c5ab136afbe73f0626f0715c0a5588
Yuvika Chaudhary <yuvikac84@gmail.com>
2025-04-11 22:40:03 +0530

    Fixed repo structure

--- spreadsheet-rust/rustlab/target/debug/deps/rustlab-d577a930daffc82c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Adit Jindal\OneDrive\Desktop\Acads-IIT\COP290\spreadsheet-rust\rustlab\target\debug\deps\librustlab-d577a930daffc82c.rmeta: src\main.rs src\parser.rs
-
-C:\Users\Adit Jindal\OneDrive\Desktop\Acads-IIT\COP290\spreadsheet-rust\rustlab\target\debug\deps\rustlab-d577a930daffc82c.d: src\main.rs src\parser.rs
-
-src\main.rs:
-src\parser.rs:
--- spreadsheet-rust/rustlab/target/debug/deps/rustlab.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Adit Jindal\OneDrive\Desktop\Acads-IIT\COP290\spreadsheet-rust\rustlab\target\debug\deps\rustlab.exe: src\main.rs src\parser.rs
-
-C:\Users\Adit Jindal\OneDrive\Desktop\Acads-IIT\COP290\spreadsheet-rust\rustlab\target\debug\deps\rustlab.d: src\main.rs src\parser.rs
-
-src\main.rs:
-src\parser.rs:
