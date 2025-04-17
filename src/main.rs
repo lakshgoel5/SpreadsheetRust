bc7adc4f93758d5e370cdfdd981bc670c958be29
Laksh Goel <lakshgoel541@gmail.com>
2025-04-17 13:57:55 +0000

    Checking yml file

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,16 @@ fn main() {
     }
     let rows: usize = args[1].parse().expect("Invalid number of rows");
     let columns: usize = args[2].parse().expect("Invalid number of columns");
-    if rows>999 || columns>18278 {
-        eprintln! ("Invalid input: rows and cols need to be within 999 and ZZZ respectively");
+    if rows > 999 || columns > 18278 {
+        eprintln!("Invalid input: rows and cols need to be within 999 and ZZZ respectively");
         std::process::exit(1);
     }
 
     // reading command and replacing the trailing newline with null character
     let mut cmd = String::new();
-    let _bytes_read = std::io::stdin().read_line(&mut cmd).expect("Failed to read command");
+    let _bytes_read = std::io::stdin()
+        .read_line(&mut cmd)
+        .expect("Failed to read command");
     let cmd = String::from(cmd.trim());
 
     // calling parser
